dd7342921ba1e7c48def798defc97a362ab137a9
erxclau <ericlau41802@gmail.com>
2022-04-17 01:53:20 -0400

    Export build function

--- src/bore.js
+++ src/bore.js
@@ -2,3 +2,7 @@ export * from "./util/color";
 export { default as wrap } from "./util/wrap";
 export { default as BarChart } from "./graphic/BarChart";
 export { default as GroupedBarChart } from "./graphic/GroupedBarChart";
+
+const build = (chart) => chart.draw.bind(chart);
+
+export { build };
