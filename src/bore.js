482c800553500d0a7fe6c7610b4d5dd60ec15ace
erxclau <ericlau41802@gmail.com>
2022-04-16 23:34:20 -0400

    Add grouped bar chart

--- src/bore.js
+++ src/bore.js
@@ -1,3 +1,4 @@
 export * from "./util/color";
 export { default as wrap } from "./util/wrap";
 export { default as BarChart } from "./graphic/BarChart";
+export { default as GroupedBarChart } from "./graphic/GroupedBarChart";
