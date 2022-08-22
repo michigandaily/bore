e7d04117cd9a39bfe6a8be999a3f68a06f7a4f3a
erxclau <ericlau41802@gmail.com>
2022-08-21 22:06:09 -0400

    Set new yAxisLeft

--- src/util/axis.js
+++ src/util/axis.js
@@ -2,9 +2,11 @@ import { axisTop, axisLeft, axisBottom } from "d3";
 
 export const xAxisTop = function (scale) {
   return (g) => {
+    const { top, bottom } = this.margin();
     const axis = axisTop(scale).ticks(this.getResponsiveWidth() / 80);
     const selection = this.getSelectionWithRedrawContext(g);
     selection.call(axis);
+    selection.selectAll(".tick line").attr("y2", this.height() - top - bottom);
   };
 };
 
@@ -21,6 +23,10 @@ export const xAxisBottom = function (scale) {
 export const yAxisLeft = function (scale) {
   return (g) => {
     const selection = this.getSelectionWithRedrawContext(g);
-    selection.call(axisLeft(scale).tickSize(0));
+    selection.call(axisLeft(scale));
+
+    const { left, right } = this.margin();
+    const w = this.getResponsiveWidth();
+    selection.selectAll(".tick line").attr("x2", w - left - right);
   };
 };
