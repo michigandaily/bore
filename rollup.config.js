be81e44b45432d885540bfa8c996164aa0ebb426
erxclau <ericlau41802@gmail.com>
2022-12-02 23:46:29 -0500

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import scss from "rollup-plugin-scss";
 import pkg from "./package.json";
 
